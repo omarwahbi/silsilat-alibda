ed0abc9d819cf2a659231fa68699f2ffc97d09b1
omarwahbi <99210606+omarwahbi@users.noreply.github.com>
2023-12-18 00:30:19 +0300

    added vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./Pages/Home";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Analytics />
     </div>
   );
 }
